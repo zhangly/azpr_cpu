cd3abec96057ee0fa1ef802c2941ac37a1800588
zhangly <zly0123@gmail.com>
2014-08-10 19:39:20 +0800

    8.10

--- azpr_asm/loader.asm
+++ azpr_asm/loader.asm
@@ -1,3 +1,7 @@
+;;; 1、编译：azprasm led.asm -o led.bin --coe led.coe
+;;; 2、手工将Xilinx FPGA的coe文件转换为Altera FPGA的mif格式作为ROM初始化数据文件 sprom16.mif
+;;; 3、开发板综合时，将sprom16.mif作为ROM的初始化数据文件
+
 ;;; 定义 
 UART_BASE_ADDR_H	EQU		0x6000		;UART Base Address High
 UART_STATUS_OFFSET	EQU		0x0			;UART Status Register Offset
@@ -249,3 +253,4 @@ _WAIT_PUSH_SW_OFF:
 _WAIT_PUSH_SW_RETURN:
 	JMP		r31
 	ANDR	r0,r0,r0					;NOP
+
