81f084dc4e0a894d5e76f676cf359564d593902d
zhangly <zly0123@gmail.com>
2014-08-10 20:11:11 +0800

    8.10

--- azpr_asm/loader.asm
+++ azpr_asm/loader.asm
@@ -1,4 +1,7 @@
 ;;; 程序加载器
+;;; 1、编译  azprasm loader.asm -o loader.bin --coe loader.coe
+;;; 2、手工将Xilinx FPGA的coe文件转换为Altera FPGA的mif格式作为ROM初始化数据文件 loader16.mif
+;;; 3、开发板综合时，将loader16.mif作为ROM的初始化数据文件
 
 ;;; 符号定义
 UART_BASE_ADDR_H	EQU		0x6000		;UART Base Address High
